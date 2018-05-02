dfd82c18953af87ed5bbedf8bd058a524f299afb
NexturnZ <ZhouLKmail@gmail.com>
2018-05-02 18:40:24 +0800

    improved display format

--- Scramble.m
+++ Scramble.m
@@ -1,4 +1,4 @@
-clear;
+clear all;
 
 init = zeros(1,7);
 data = ones(1,100);
@@ -9,10 +9,10 @@ for i1 = 2:100
    [Scrdt(i1), reg] =  Scrambler(data(i1), reg);
 end
 
-fprintf('origin\tresult\n');
+fprintf('origin\t\t\tScrambled data\t\tDescrabled data\n');
 rec = zeros(1,100);
 [rec(1),reg] = Descrambler(Scrdt(1), init);
 for i1 = 2:100
    [rec(i1), reg] =  Descrambler(Scrdt(i1), reg);
-   fprintf('%d\t%d\n',data(i1),rec(i1));
+   fprintf('%d\t\t\t\t%d\t\t\t\t\t%d\n',data(i1),Scrdt(i1),rec(i1));
 end
